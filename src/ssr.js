d0ccad888b3d89296c94ca0675fd729f56155822
Kiryl Parfiankou <kiryl.parfiankou@itechart-group.com>
2019-08-27 08:50:23 +0300

    update ssr

--- src/ssr.js
+++ src/ssr.js
@@ -18,7 +18,14 @@ async function ssr(url) {
   await page.evaluate(() => {
     document.querySelector(`script[src="https://get.mavo.io/mavo.es5.min.js"]`).remove();
     document.querySelector(`link[href="https://get.mavo.io/mavo.css"]`).remove();
+    document.querySelector(`link[href="https://dmitrysharabin.github.io/mavo-cropper/mavo-cropper.css"]`).remove();
+    document.querySelector(`link[href="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.1/cropper.min.css"]`).remove();
+    document.querySelector(`script`).remove();
     document.querySelector(`.mv-bar.mv-ui`).remove();
+    const list = document.querySelector('.carusel__list');
+    if (list) {
+      list.style.setProperty('--shift', `0px`);
+    }
   });
 
   const html = await page.content(); 
